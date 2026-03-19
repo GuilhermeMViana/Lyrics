b973e4d272cd073b0918d47c9bcb807b8e6f99fb
GuilhermeMViana <gmunizviana@gmail.com>
2026-03-19 13:05:49 -0300

    Initial Commit

---
+++ LyricsTests/LyricsTests.swift
@@ -0,0 +1,17 @@
+//
+//  LyricsTests.swift
+//  LyricsTests
+//
+//  Created by Guilherme Muniz Viana on 19/03/26.
+//
+
+import Testing
+@testable import Lyrics
+
+struct LyricsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
