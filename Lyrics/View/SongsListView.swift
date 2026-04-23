4fd268c0a4543ccf3951265e8f240ed01ebe3326
GuilhermeMViana <gmunizviana@gmail.com>
2026-04-23 19:46:55 -0300

    feat(SongsListView): simple view to test api return

--- Lyrics/View/SongsListView.swift
+++ Lyrics/View/SongsListView.swift
@@ -8,10 +8,49 @@
 import SwiftUI
 
 struct SongsListView: View {
+    @State private var results = [Result]()
+    @State private var search = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List(results, id: \.trackId) { item in
+                VStack(alignment: .leading) {
+                    Text(item.trackName)
+                        .font(.headline)
+                    Text(item.artistName)
+                }
+            }
+            .task {
+                await loadData()
+            }
+        }
+        .searchable(text: $search)
+        .onChange(of: search) { oldValue, newValue in
+            Task {
+                await loadData()
+            }
+        }
+    }
+    
+    func loadData() async {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=\(search)&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                results = decodedResponse.results
+            }
+        } catch {
+            print("Invalid data")
+        }
     }
 }
+
+
 
 #Preview {
     SongsListView()
